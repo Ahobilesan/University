c7718601cfb16fd6c7a2625eb1c78fc8edb7a775
Ahobilesan <ahobilesan.gurumurthy@gmail.com>
2021-05-16 17:53:28 +0530

    data file updated

--- src/assets/data.ts
+++ src/assets/data.ts
@@ -32,3 +32,21 @@ export const Grades = [
     { "key": "D", "value": "D", "text": "D for Percent 65-66" },
     { "key": "E/F", "value": "E/F", "text": "E/F for Percent 65" }
 ]
+
+export const GradeMap = [
+    { "Letter Grade": "A+", "Percent Grade": "97-100", "4.0 Scale": "4.0" },
+    { "Letter Grade": "A", "Percent Grade": "93-96 ", "4.0 Scale": "4.0" },
+    { "Letter Grade": "A-", "Percent Grade": "90-92 ", "4.0 Scale": "3.7" },
+    { "Letter Grade": "B+", "Percent Grade": "87-89 ", "4.0 Scale": "3.3" },
+    { "Letter Grade": "B", "Percent Grade": "83-86 ", "4.0 Scale": "3.0" },
+    { "Letter Grade": "B-", "Percent Grade": "80-82 ", "4.0 Scale": "2.7" },
+    { "Letter Grade": "C+", "Percent Grade": "77-79 ", "4.0 Scale": "2.3" },
+    { "Letter Grade": "C", "Percent Grade": "73-76 ", "4.0 Scale": "2.0" },
+    { "Letter Grade": "C-", "Percent Grade": "70-72 ", "4.0 Scale": "1.7" },
+    { "Letter Grade": "D+", "Percent Grade": "67-69 ", "4.0 Scale": "1.3" },
+    { "Letter Grade": "D", "Percent Grade": "65-66 ", "4.0 Scale": "1.0" },
+    { "Letter Grade": "E/F", "Percent Grade": "65", "4.0 Scale": "0.0" }
+]
+
+export const Months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+export const Days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
